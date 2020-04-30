8b44bfbad54513b31556740a8293be70e78e8d91
Eloy <eloyclemente@gmail.com>
2020-04-30 15:08:23 +0200

    Updates

---
+++ precache-manifest.b284805a4740aebbf5b7bddf7b92859e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe0a449f2e0548411200b9e10b503f8",
+    "url": "/react-prueba-1/index.html"
+  },
+  {
+    "revision": "0ef6e5835b509763cdfd",
+    "url": "/react-prueba-1/static/css/main.4d8a76a5.chunk.css"
+  },
+  {
+    "revision": "ea3ff1a09d5d4c885b99",
+    "url": "/react-prueba-1/static/js/2.cd1a56eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-prueba-1/static/js/2.cd1a56eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef6e5835b509763cdfd",
+    "url": "/react-prueba-1/static/js/main.c4a9d505.chunk.js"
+  },
+  {
+    "revision": "19a0130510e7d1311429",
+    "url": "/react-prueba-1/static/js/runtime-main.b87c8221.js"
+  }
+]);
