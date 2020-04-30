7a499839c9d91477ed5b4a2d9010cf4217c0192a
Eloy <eloyclemente@gmail.com>
2020-04-30 15:11:55 +0200

    Second

--- src/views/Contacto.js
+++ src/views/Contacto.js
@@ -1,44 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 
 function Contacto()
 {
-	const [ datos, setDatos ] = useState({ user: '', pass: '' })
-	const [ imprimir, setImprimir ] = useState('vacio')
-
-
-
-	function handleInputChange(event)
-	{
-		setDatos({ ...datos, [event.target.name] : event.target.value })
-	}
-
-
-	function enviarDatos(event)
-	{
-		event.preventDefault()
-		setImprimir([ datos.user ] + [ datos.pass ])
-	}
-
-
 	return(
-		<div>
-			<p>Página de contacto</p>
-			<form onSubmit={ enviarDatos }>
-				<input type="text" 
-					  name="user" 
-					  placeholder="Nombre"
-					  onChange={ handleInputChange }
-				/>
-				<input type="password" 
-					  name="pass" 
-					  placeholder="Contraseña"
-					  onChange={ handleInputChange }
-				/>
-				<input type="submit" value="Enviar"/>
-			</form>
-			<p>{ imprimir }</p>
-		</div>
+		<p>Página de contacto</p>
 	)
 }
 
--- src/views/Registro.js
+++ src/views/Registro.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState }  from 'react'
 
-const Registro = () =>
+function registro()
 {
+	const [ datos, setDatos ] = useState({ user: '', pass: '' })
+	const [ imprimir, setImprimir ] = useState('vacio')
+
+
+
+	function handleInputChange(event)
+	{
+		setDatos({ ...datos, [event.target.name] : event.target.value })
+	}
+
+
+	function enviarDatos(event)
+	{
+		event.preventDefault()
+		setImprimir([ datos.user ] + [ datos.pass ])
+	}
+
+
 	return(
 		<div>
-			<p>Página de registro</p>
+			<p>Página de contacto</p>
+			<form onSubmit={ enviarDatos }>
+				<input type="text" 
+					  name="user" 
+					  placeholder="Nombre"
+					  onChange={ handleInputChange }
+				/>
+				<input type="password" 
+					  name="pass" 
+					  placeholder="Contraseña"
+					  onChange={ handleInputChange }
+				/>
+				<input type="submit" value="Enviar"/>
+			</form>
+			<p>{ imprimir }</p>
 		</div>
 	)
 }
