435597155d1043c0849c77f0beca4a49e5376a4f
Eloy <eloyclemente@gmail.com>
2020-04-30 17:20:28 +0200

    Firebase

--- src/views/Inicio.js
+++ src/views/Inicio.js
@@ -5,6 +5,21 @@ const Inicio = () =>
 	return(
 		<div>
 			<p>Página de inicio</p>
+			<br/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+				Odio quae expedita magnam tenetur maxime mollitia officiis facilis repellat 
+				repellendus qui ut, natus id in eum, temporibus deleniti eos laboriosam quod?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+				Odio quae expedita magnam tenetur maxime mollitia officiis facilis repellat 
+				repellendus qui ut, natus id in eum, temporibus deleniti eos laboriosam quod?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+				Odio quae expedita magnam tenetur maxime mollitia officiis facilis repellat 
+				repellendus qui ut, natus id in eum, temporibus deleniti eos laboriosam quod?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+				Odio quae expedita magnam tenetur maxime mollitia officiis facilis repellat 
+				repellendus qui ut, natus id in eum, temporibus deleniti eos laboriosam quod?
+			</p>
 		</div>
 	)
 }
